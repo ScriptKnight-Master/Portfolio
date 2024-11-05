9add23157281f667f5899e5ed06bcdbc4fb6a313
Script Knight <ScriptKnight.Master@hotmail.com>
2024-11-04 20:47:21 -0800

    Fixed merge conflict error

--- src/app/pages/home/index.js
+++ src/app/pages/home/index.js
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import { styled } from "@mui/material/styles";
 import { useSpring, animated } from "react-spring";
+import { Tooltip } from "react-tooltip";
 // import { useDispatch } from "react-redux";
 // import { toggleTheme } from "store/themeSlice";
+import LeftMenu from "./leftmenu.js";
 import { IconContext } from "react-icons";
 import { BsWindows } from "react-icons/bs";
+import { FaWindowMinimize } from "react-icons/fa6";
+import { FaRegSquare } from "react-icons/fa6";
+import { RiCloseLargeLine } from "react-icons/ri";
+
 import "swiper/css/bundle";
+
 const Root = styled("div")(({ theme }) => ({
   color: theme.palette.primary.main,
   position: "absolute",
@@ -15,6 +22,7 @@ const Root = styled("div")(({ theme }) => ({
   padding: theme.spacing(1),
   "& .themeborder": {
     borderColor: theme.portfolio.palette.primary.border,
+    color: theme.portfolio.palette.primary.border,
   },
   "& .frame-main": {
     display: "grid",
@@ -27,6 +35,9 @@ const Root = styled("div")(({ theme }) => ({
     gridArea: "header",
     gridTemplateColumns: "subgrid",
   },
+  "& .hero": {
+    height: "calc(100vh - 6rem)",
+  },
   "& .scrollbar-custom": {
     scrollbarColor: "green transparent",
     scrollbarWidth: "thin",
@@ -34,12 +45,34 @@ const Root = styled("div")(({ theme }) => ({
   "& .HomeIcon": {
     color: theme.portfolio.palette.primary.border,
     fontSize: "1rem",
-    transitionDuration: "0.7s",
+    transitionDuration: "0.5s",
     cursor: "pointer",
   },
   "& .HomeIcon:hover": {
     color: "#00aaf7",
-    transitionDuration: "0.7s",
+    transitionDuration: "0.5s",
+  },
+  "& .tool-header": {
+    color: theme.portfolio.palette.primary.border,
+    transitionDuration: "0.5s",
+    cursor: "pointer",
+  },
+  "& .tool-header:hover": {
+    color: "#00aaf7",
+    transitionDuration: "0.5s",
+  },
+  "& .toolIcon": {
+    color: theme.portfolio.palette.primary.border,
+    fontSize: "0.8rem",
+    transitionDuration: "0.3s",
+    cursor: "pointer",
+    margin: "5px",
+    marginTop: "3px",
+    marginBottom: "3px",
+  },
+  "& .toolIcon:hover": {
+    color: "#cccccc",
+    transitionDuration: "0.3s",
   },
 }));
 
@@ -53,21 +86,27 @@ const Home = () => {
           </IconContext.Provider>
         </div>
         <nav className="hidden items-center justify-center md:flex">
-          <ul>
-            <li>asdf</li>
-            <li>asdf</li>
-            <li>asdf</li>
-            <li>asdf</li>
-            <li>asdf</li>
-          </ul>
+          <LeftMenu />
         </nav>
       </header>
       <div className="themeborder grid grid-cols-3 items-center gap-4 border-b border-base-300 px-10 glossy [grid-area:top] md:grid">
         <div></div>
-        <div>&lt;ScriptKnight.Master/&gt;</div>
-        <div>VVVV</div>
+        <div className="tool-header font-700 text-sm text-center cursor-pointer">
+          &lt;ScriptKnight.Master/&gt;
+        </div>
+        <div className="flex flex-row justify-end">
+          <IconContext.Provider value={{ className: "toolIcon" }}>
+            <FaWindowMinimize />
+          </IconContext.Provider>
+          <IconContext.Provider value={{ className: "toolIcon" }}>
+            <FaRegSquare />
+          </IconContext.Provider>
+          <IconContext.Provider value={{ className: "toolIcon" }}>
+            <RiCloseLargeLine />
+          </IconContext.Provider>
+        </div>
       </div>
-      <div className="scrollbar-custom relative overflow-x-hidden overflow-y-scroll [grid-area:content] lenis">
+      <div className="scrollbar-custom hero relative overflow-x-hidden overflow-y-scroll [grid-area:content]">
         <div className="w-100 mb-48">asdfa</div>
         <div className="w-100 mb-48">asdfa</div>
         <div className="w-100 mb-48">asdfa</div>
